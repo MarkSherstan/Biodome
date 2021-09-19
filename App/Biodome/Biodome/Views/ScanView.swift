6e8c0339ccf0268d1e87c97487b76f00ff34cd0d
Mark Sherstan <mshersta@ualberta.ca>
2021-09-19 17:57:35 -0600

    Prep for toggle switch and change list to be more general with a struct view and header.

--- App/Biodome/Biodome/Views/ScanView.swift
+++ App/Biodome/Biodome/Views/ScanView.swift
@@ -14,43 +14,63 @@ struct ScanView: View {
     var body: some View {
         NavigationView {
             VStack{
-                // List of Devices
-                List(bleManager.peripherals) { peripheral in
-                    NavigationLink(destination: ContentView(Name: peripheral.name, ID: peripheral.id)){
-                        HStack {
-                            Text(peripheral.name)
-                            Spacer()
-                            Text(String(peripheral.rssi))
+                // Scanning toggle switch
+                Toggle(isOn: /*@START_MENU_TOKEN@*/.constant(true)/*@END_MENU_TOKEN@*/){
+                    Text("Scan")
+                }.padding()
+                
+                Spacer()
+                
+                // Found Device List
+                List {
+                    Section(header: Text("Devices")) {
+                        ForEach(bleManager.peripherals) { peripheral in
+                            PeripheralRow(peripheral: peripheral)
                         }
                     }
-                }.frame(height: 600)
-                Spacer()
+                }.listStyle(GroupedListStyle())
                 
                 // Scanning Buttons
                 HStack {
                     Button(action: {self.bleManager.startScanning()})
-                        {Text("Start Scanning")
+                        {Text("Start Scan")
                     }
-                    
+
                     Spacer()
-                    
+
                     Button(action: {self.bleManager.stopScanning()}) {
                         Text("Stop Scanning")
                     }
                 }.padding()
                 
                 // Status
-                if bleManager.isSwitchedOn {
-                    Text("Bluetooth is ON")
-                        .foregroundColor(.green)
-                } else {
-                    Text("Bluetooth is OFF")
-                        .foregroundColor(.red)
-                }
-            }.navigationBarTitle("Bluetooth Devices", displayMode: .inline)
+//                if bleManager.isSwitchedOn {
+//                    Text("Bluetooth is ON")
+//                        .foregroundColor(.green)
+//                } else {
+//                    Text("Bluetooth is OFF")
+//                        .foregroundColor(.red)
+//                }
+                
+            } .navigationBarTitle("Device Select") //, displayMode: .inline
         }.environmentObject(bleManager)
     }
 }
+
+struct PeripheralRow: View {
+    var peripheral: Peripheral
+    
+    var body: some View {
+        NavigationLink(destination: ContentView(Name: peripheral.name, ID: peripheral.id)){
+            HStack {
+                Text(peripheral.name)
+                Spacer()
+                Text(String(peripheral.rssi))
+            }
+        }
+    }
+}
+
 
 struct ScanView_Previews: PreviewProvider {
     static var previews: some View {
