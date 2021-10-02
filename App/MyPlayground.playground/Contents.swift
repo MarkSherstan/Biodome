8b6507ea94ebc2c271953a95ca575a5ee155b4dd
Mark Sherstan <mshersta@ualberta.ca>
2021-10-02 14:35:59 -0600

    Empty the playground.

--- App/MyPlayground.playground/Contents.swift
+++ App/MyPlayground.playground/Contents.swift
@@ -1,17 +1,1 @@
 import UIKit
-
-var greeting = "Hello, playground"
-
-var sensors = ["Humidity 1: ", "Humidity 2: ", "Light: ", "Temperature: "]
-
-print(sensors.count)
-
-for sensor in sensors{
-    print(sensor)
-}
-
-for ii in 1...sensors.count{
-    print(ii)
-}
-
-print(sensors)
