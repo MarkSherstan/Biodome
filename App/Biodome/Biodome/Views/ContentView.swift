919f9880c2246826e0066f5789056ad200eed4ae
Mark Sherstan <mshersta@ualberta.ca>
2021-12-08 22:58:02 -0700

    Tidy up

--- App/Biodome/Biodome/Views/ContentView.swift
+++ App/Biodome/Biodome/Views/ContentView.swift
@@ -7,75 +7,6 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @StateObject var bleManager = BLEManager()
-    @State private var sheetState = false
-    
-    var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Biodome")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Text(bleManager.connectionState)
-                .font(.subheadline)
-            
-            Divider()
-            
-            // Sensor values
-            ScrollView {
-                LazyVGrid(columns: gridItemLayout, spacing: 20){
-                    
-                    Widget(title: "TEMPERATURE A", value: bleManager.temperature, units: "°C", imageName: "thermometer", widgetColor: .green)
-                    
-                    Widget(title: "TEMPERATURE B", value: bleManager.temperature, units: "°C", imageName: "thermometer", widgetColor: .green)
-
-                    Widget(title: "LIGHT", value: bleManager.sunIntensity, units: " lux", imageName: "sun.max", widgetColor: .orange)
-
-                    Widget(title: "MOISTURE", value: bleManager.soilMoisture, units: "%", imageName: "humidity", widgetColor: .teal)
-                    }
-                }
-            
-            // Discover button
-            HStack(){
-                Spacer()
-                
-                Button(action: {
-                    sheetState.toggle()
-                    self.bleManager.toggleScanning(scanState: true)
-                }) {
-                    Image(systemName: "magnifyingglass")
-                    Text("Discover")
-                }
-                .sheet(isPresented: $sheetState,
-                       onDismiss: {self.bleManager.toggleScanning(scanState: false)},
-                       content:{SheetView()
-                })
-                
-                Spacer()
-            }
-
-        }
-        .environmentObject(bleManager)
-        .padding()
-    }
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            // Dark View
-            ContentView().preferredColorScheme(.dark)
-            
-            // Light View
-            ContentView().preferredColorScheme(.light)
-        }
-    }
-}
-
-
 struct Widget: View {
     @EnvironmentObject var bleManager: BLEManager
     var title: String
@@ -85,21 +16,21 @@ struct Widget: View {
     var widgetColor: Color
     
     var body: some View {
-        ZStack(alignment: .top){
+        ZStack(alignment: .top) {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
                 .strokeBorder(widgetColor, lineWidth: 3)
                 .background( RoundedRectangle(cornerRadius: 25, style: .continuous).foregroundColor(widgetColor))
                 .opacity(0.3)
                 .frame(width: 150, height: 100)
             
-            VStack(spacing: 10){
-                HStack(){
+            VStack(spacing: 10) {
+                HStack() {
                     Image(systemName: imageName)
                         .resizable()
                         .scaledToFit()
                         .frame(width: 15, height: 15)
                         .foregroundColor(widgetColor)
-                        
+                    
                     Text(title)
                         .font(.footnote)
                         .foregroundColor(widgetColor)
@@ -118,5 +49,68 @@ struct Widget: View {
                 }
             }
         }
+    }
+}
+
+
+struct ContentView: View {
+    @StateObject var bleManager = BLEManager()
+    @State private var sheetState = false
+    
+    var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Biodome")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text(bleManager.connectionState)
+                .font(.subheadline)
+            
+            Divider()
+            
+            // Sensor values
+            ScrollView {
+                LazyVGrid(columns: gridItemLayout, spacing: 20) {
+                    Widget(title: "TEMPERATURE A", value: bleManager.temperature, units: "°C", imageName: "thermometer", widgetColor: .green)
+                    Widget(title: "TEMPERATURE B", value: bleManager.temperature, units: "°C", imageName: "thermometer", widgetColor: .green)
+                    Widget(title: "LIGHT", value: bleManager.sunIntensity, units: " lux", imageName: "sun.max", widgetColor: .orange)
+                    Widget(title: "MOISTURE", value: bleManager.soilMoisture, units: "%", imageName: "humidity", widgetColor: .teal)
+                }
+            }
+            
+            // Discover button
+            HStack() {
+                Spacer()
+                
+                Button(action: {
+                    sheetState.toggle()
+                    self.bleManager.toggleScanning(scanState: true)
+                }) {
+                    Image(systemName: "magnifyingglass")
+                    Text("Discover")
+                }
+                .sheet(isPresented: $sheetState,
+                       onDismiss: {self.bleManager.toggleScanning(scanState: false)},
+                       content:{SheetView()
+                })
+                
+                Spacer()
+            }
+            
+        }
+        .environmentObject(bleManager)
+        .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Dark View
+        ContentView().preferredColorScheme(.dark)
+        
+        // Light View
+        ContentView().preferredColorScheme(.light)
     }
 }
