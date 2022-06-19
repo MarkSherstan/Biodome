8a07387e688348c23342730007cf4370901653ea
MarkSherstan <mshersta@ualberta.ca>
2022-06-19 11:53:29 -0600

    Simplify publish

--- Thing/publish.py
+++ Thing/publish.py
@@ -1,18 +1,9 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0.
-
 from awscrt import mqtt
 import sys
 import threading
 import time
 from uuid import uuid4
 import json
-
-# This sample uses the Message Broker for AWS IoT to send and receive messages
-# through an MQTT connection. On startup, the device connects to the server,
-# subscribes to a topic, and begins publishing messages to that topic.
-# The device should receive those same messages back from the message broker,
-# since it is subscribed to that same topic.
 
 # Parse arguments
 import command_line_utils;
@@ -25,12 +16,14 @@ cmdUtils.register_command("key", "<path>", "Path to your key in PEM format.", Tr
 cmdUtils.register_command("cert", "<path>", "Path to your client certificate in PEM format.", True, str)
 cmdUtils.register_command("port", "<int>", "Connection port. AWS IoT supports 433 and 8883 (optional, default=auto).", type=int)
 cmdUtils.register_command("client_id", "<str>", "Client ID to use for MQTT connection (optional, default='test-*').", default="test-" + str(uuid4()))
-cmdUtils.register_command("count", "<int>", "The number of messages to send (optional, default='10').", default=10, type=int)
-# Needs to be called so the command utils parse the commands
 cmdUtils.get_args()
 
+message_count = 3
+message_topic = "topic_1"
+message_string = "123"
 received_count = 0
 received_all_event = threading.Event()
+
 
 # Callback when connection is accidentally lost.
 def on_connection_interrupted(connection, error, **kwargs):
@@ -78,9 +71,9 @@ if __name__ == '__main__':
     connect_future.result()
     print("Connected!")
 
-    message_count = cmdUtils.get_command("count")
-    message_topic = cmdUtils.get_command(cmdUtils.m_cmd_topic)
-    message_string = cmdUtils.get_command(cmdUtils.m_cmd_message)
+    # message_count = cmdUtils.get_command("count")
+    # message_topic = cmdUtils.get_command(cmdUtils.m_cmd_topic)
+    # message_string = cmdUtils.get_command(cmdUtils.m_cmd_message)
 
     # Subscribe
     print("Subscribing to topic '{}'...".format(message_topic))
